5521f6ec64eecf3622fa9040a94314ea96c75ffa
Bryan Myung <bryanmyung@gmail.com>
2020-04-08 15:17:45 -0700

    without socket

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+// exports.bigben = functions.https.onRequest((req, res) => {
+//   const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+//   res.status(200).send(`<!doctype html>
+//     <head>
+//       <title>Time</title>
+//     </head>
+//     <body>
+//       ${'BONG '.repeat(hours)}
+//     </body>
+//   </html>`);
+// });
+
+app.get('/', (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+  res.send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
+    </head>
+    <body>
+      <p>In London, the clock strikes:
+        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
+    </body>
+  </html>`);
+});
--- welcometo_r/src/components/section/goals.js
+++ welcometo_r/src/components/section/goals.js
@@ -5,7 +5,7 @@ export class Goal extends React.Component {
     render() {
         return (
             <div className='goalCard'>
-                <h2> Level {this.props.goalCard.level}</h2>
+                <h2> n' {this.props.goalCard.level}</h2>
                     <div className='goalDetail'>
                         <h1>{this.props.goalCard.goal}</h1>
                         <h3>{this.props.goalCard.description}</h3>
